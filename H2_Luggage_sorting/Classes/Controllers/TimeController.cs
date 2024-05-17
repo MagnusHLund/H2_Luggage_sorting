1e345e588b9a1dc457ae0c723669893096918588
Robert Pedersen <128043312+Robes13@users.noreply.github.com>
2024-05-17 16:49:09 +0200

    Add files via upload

---
+++ H2_Luggage_sorting/Classes/Controllers/TimeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Luggage_sorting.Classes.Controllers
+{
+    internal class TimeController
+    {
+        #region Fields
+
+        const int ticks = 500;
+
+        #endregion
+
+        #region Constructors
+
+
+
+        #endregion
+
+        #region Methods
+        public DateTime AddMinuteToDate(DateTime myDate)
+        {
+            myDate.AddMinutes(1);
+            Thread.Sleep(ticks);
+
+            return myDate;
+        }
+        #endregion
+    }
+}
